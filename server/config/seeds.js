151b11c30c318047d3087b93f337f3373c3b4056
Shanna-not-Shawna <144177634+Shanna-not-Shawna@users.noreply.github.com>
2023-12-03 10:51:56 -0600

    added quantity back to seed data.

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -26,6 +26,7 @@ db.once('open', async () => {
       image: 'merryandbright.png',
       category: categories[0]._id,
       price: .99,
+      quantity: 500
     },
     {
       name: 'Obligatory Gesture',
@@ -34,6 +35,7 @@ db.once('open', async () => {
       image: 'obligatorygesture.png',
       category: categories[0]._id,
       price: .99,
+      quantity: 500
     },
     {
       name: 'Merry and Bright 2',
@@ -42,6 +44,7 @@ db.once('open', async () => {
         'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
       image: 'merryandbright.png',
       price: .99,
+      quantity: 500
     },
     {
       name: 'Obligatory Gesture 2',
@@ -50,6 +53,7 @@ db.once('open', async () => {
         'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
       image: 'obligatorygesture.png',
       price: .99,
+      quantity: 500
     },
     {
       name: 'Merry and Bright 3',
@@ -58,6 +62,7 @@ db.once('open', async () => {
         'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
       image: 'merryandbright.png',
       price: .99,
+      quantity: 500
     },
     {
       name: 'Obligatory Gesture 3',
@@ -66,6 +71,7 @@ db.once('open', async () => {
         'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
       image: 'obligatorygesture.png',
       price: .99,
+      quantity: 500
     },
     {
       name: 'Merry and Bright 4',
@@ -74,6 +80,7 @@ db.once('open', async () => {
         'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
       image: 'merryandbright.png',
       price: .99,
+      quantity: 500
     },
     {
       name: 'Obligatory Gesture 4',
@@ -82,6 +89,7 @@ db.once('open', async () => {
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
       image: 'obligatorygesture.png',
       price: .99,
+      quantity: 500
     },
     {
       name: 'Merry and Bright 5',
@@ -89,6 +97,7 @@ db.once('open', async () => {
       description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
       image: 'merryandbright.png',
       price: .99,
+      quantity: 500
     },
     {
       name: 'Obligatory Gesture 5',
@@ -97,6 +106,7 @@ db.once('open', async () => {
         'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
       image: 'obligatorygesture.png',
       price: .99,
+      quantity: 500
     },
     {
       name: 'Merry and Bright 6',
@@ -105,6 +115,7 @@ db.once('open', async () => {
         'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
       image: 'merryandbright.png',
       price: .99,
+      quantity: 500
     },
     {
       name: 'Obligatory Gesture 6',
@@ -113,6 +124,7 @@ db.once('open', async () => {
         'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
       image: 'obligatorygesture.png',
       price: .99,
+      quantity: 500
     }
   ]);
 
