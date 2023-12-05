3b4e0c6f74c51ac7b1932547511e61638014c374
kevinsmithseven <kevinsmithseven@gmail.com>
2023-12-05 11:08:57 -0600

    updated auth.js to protect secret key

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,9 @@
 const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
+require('dotenv').config()
 
-const secret = 'mysecretssshhhhhhh';
+const secret = process.env.SECRET_DB;
+console.log(process.env.SECRET_DB);
 const expiration = '2h';
 
 module.exports = {
