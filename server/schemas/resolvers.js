d5ee497e446967451a6bd75ed82f066f3addf942
kevinsmithseven <kevinsmithseven@gmail.com>
2023-12-05 11:04:16 -0600

    updated Stripe keys and created .env files to protect

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,7 @@
 const { User, Product, Category, Order } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+require('dotenv').config()
+const stripe = require('stripe')(process.env.SECRETKEY);
 
 const resolvers = {
     Query: {
@@ -19,7 +20,7 @@ const resolvers = {
                     $regex: name
                 };
             }
-            console.log(await Product.find(params).populate('category'))
+            // console.log(await Product.find(params).populate('category'))
             return await Product.find(params).populate('category');
         },
         product: async (parent, { _id }) => {
