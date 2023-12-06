72784e90c3254fb0a03a2b398ae518344b8aa10b
Shanna-not-Shawna <144177634+Shanna-not-Shawna@users.noreply.github.com>
2023-12-06 10:03:00 -0600

    added sendEmail.js to server/utils

---
+++ server/utils/sendEmail.js
@@ -0,0 +1,52 @@
+const nodemailer = require("nodemailer");
+
+// add bccEmail back for BCC
+const sendEmail = async (subject, message, send_to, sent_from, reply_to, imageUrl, friendName, senderName, ) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: "587",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+const htmlMessage = `
+  <html>
+    <body style="margin: 0; padding: 20px; background-color: #FF0000;">
+      <div style="width: 100%; max-width: 600px; margin: 0 auto; background-color: #ffffff; padding: 20px; border-radius: 10px;">
+        <p>Dear ${friendName}</p>
+        <p>${message}</p>
+        <p>Warm regards,</p>
+        <p>${senderName}</p>
+        <img src="${imageUrl}" style="max-width: 100%;">
+      </div>
+    </body>
+  </html>
+`;
+
+  const options = {
+    from: "Seasonal Sentiments",
+    to: send_to,
+    replyTo: reply_to,
+    subject: subject,
+    html: htmlMessage,
+  };
+
+  // if (bccEmail) {
+  //   options.bcc = bccEmail;
+  // }
+
+  transporter.sendMail(options, function (err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
+};
+
+module.exports = sendEmail;
